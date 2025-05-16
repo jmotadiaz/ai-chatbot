21e7400681a5423bac8561b1c2a01e68eb2f9b43
jmotadiaz <javier.mota.dev@gmail.com>
2025-05-16 10:25:46 +0200

    remove commented code from middleware and removed register auth exception

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,5 @@
 // import { NextResponse } from "next/server";
 export { auth as middleware } from "@/app/(auth)/auth";
-
-// export default auth((req) => {
-//   const { pathname, origin } = req.nextUrl;
-//   if (!req.auth && !["/login", "/register"].includes(pathname)) {
-//     return NextResponse.redirect(new URL("/login", origin));
-//   }
-
-//   if (req.auth && ["/login", "/register"].includes(pathname)) {
-//     return NextResponse.redirect(new URL("/", origin));
-//   }
-//   return NextResponse.next();
-// });
-
 export const config = {
-  matcher: ["/((?!api|register|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
