48972b8d5bb0601882b367dc7d44027a1b70c49b
jmotadiaz <javier.mota.dev@gmail.com>
2025-05-20 14:49:20 +0200

    ignore manifest from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 // import { NextResponse } from "next/server";
 export { auth as middleware } from "@/app/(auth)/auth";
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|manifest.webmanifest).*)",
+  ],
 };
