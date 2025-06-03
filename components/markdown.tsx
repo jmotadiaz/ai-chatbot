b22cabfcc3fe3dba7b77e0d1cf930cdb59c611b3
jmotadiaz <javier.mota.dev@gmail.com>
2025-06-03 12:20:46 +0200

    improve chat headings

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -94,42 +94,51 @@ const components: Partial<Components> = {
   },
   h1: ({ node, children, ...props }) => {
     return (
-      <h1 className="animate-fade text-3xl font-semibold mt-6 mb-2" {...props}>
+      <h1 className="animate-fade text-2xl font-semibold mt-6 mb-2" {...props}>
         {children}
       </h1>
     );
   },
   h2: ({ node, children, ...props }) => {
     return (
-      <h2 className="animate-fade text-2xl font-semibold mt-4 mb-2" {...props}>
+      <h2 className="animate-fade text-xl font-semibold mt-4 mb-2" {...props}>
         {children}
       </h2>
     );
   },
   h3: ({ node, children, ...props }) => {
     return (
-      <h3 className="animate-fade text-xl font-semibold mt-4 mb-2" {...props}>
+      <h3 className="animate-fade text-lg font-semibold mt-4 mb-2" {...props}>
         {children}
       </h3>
     );
   },
   h4: ({ node, children, ...props }) => {
     return (
-      <h4 className="animate-fade text-lg font-semibold mt-4 mb-2" {...props}>
+      <h4
+        className="animate-fade text-lg opacity-80 font-semibold mt-4 mb-2"
+        {...props}
+      >
         {children}
       </h4>
     );
   },
   h5: ({ node, children, ...props }) => {
     return (
-      <h5 className="animate-fade text-base font-semibold mt-2 mb-2" {...props}>
+      <h5
+        className="animate-fade text-base opacity-80 font-semibold mt-2 mb-2"
+        {...props}
+      >
         {children}
       </h5>
     );
   },
   h6: ({ node, children, ...props }) => {
     return (
-      <h6 className="animate-fade text-sm font-semibold mt-2 mb-2" {...props}>
+      <h6
+        className="animate-fade text-base opacity-70 font-semibold mt-2 mb-2"
+        {...props}
+      >
         {children}
       </h6>
     );
