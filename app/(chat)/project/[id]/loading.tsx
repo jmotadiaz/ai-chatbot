01009ec7a9e6c279398d27fd9256839479265afb
jmotadiaz <javier.mota.dev@gmail.com>
2025-06-07 00:56:20 +0200

    improve project style

--- app/(chat)/project/[id]/loading.tsx
+++ app/(chat)/project/[id]/loading.tsx
@@ -17,7 +17,7 @@ const Loading: React.FC = () => {
           <ThemeToggle />
         </div>
       </Header>
-      <div className="h-svh flex flex-col items-center justify-center w-full max-w-2xl mx-auto pt-24 relative overflow-hidden">
+      <div className="h-svh flex flex-col items-center justify-center w-full max-w-2xl mx-auto pt-24 relative overflow-hidden px-4">
         <div className="h-7 bg-gray-200 dark:bg-zinc-700 rounded-md py-1 animate-pulse w-3/5 mb-12" />
         <div className="w-full h-[136px] bg-gray-200 dark:bg-zinc-700 rounded-lg animate-pulse px-4" />
       </div>
--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -3,8 +3,10 @@ import { useChatContext } from "@/app/providers";
 export const ProjectOverview = () => {
   const { title } = useChatContext();
   return (
-    <div className="h-full flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">{title || "AI Chatbot"}</h1>
+    <div className="h-full flex flex-col items-center justify-end px-4">
+      <h1 className="text-3xl text-center font-semibold mb-4">
+        {title || "AI Chatbot"}
+      </h1>
     </div>
   );
 };
