ccea254586c58e7185d84e58fd2577416ce0a7a3
jmotadiaz <javier.mota.dev@gmail.com>
2025-06-08 16:41:35 +0200

    change app logo

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000",
     icons: [
       {
-        src: "/app-icon.png",
+        src: "/app-logo.png",
         sizes: "512x512",
         type: "image/png",
       },
