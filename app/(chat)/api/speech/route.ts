6e399d62b83c5ab287c92d4a9bb4a693bf5f7e96
jmotadiaz <javier.mota.dev@gmail.com>
2025-06-25 08:37:58 +0200

    improve speech

--- app/(chat)/api/speech/route.ts
+++ app/(chat)/api/speech/route.ts
@@ -6,9 +6,11 @@ export async function POST(req: Request) {
 
   try {
     const result = await generateSpeech({
-      model: openai.speech("tts-1"),
+      model: openai.speech("gpt-4o-mini-tts"),
       speed: 0.9,
       voice: "alloy",
+      instructions: `When language is spanish, the pronunciation should be spanish (spain).
+        When language is english, the pronunciation should be english (uk).`,
       text: input,
     });
 
