db949690fc0fa66df9ebbbd17d4f28919e9f9653
jmotadiaz <javier.mota.dev@gmail.com>
2025-07-08 13:19:09 +0200

    securized delete resources

--- app/(chat)/api/delete-resources/route.ts
+++ app/(chat)/api/delete-resources/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from "next/server";
-import { deleteResources } from "@/lib/db/queries";
+// import { deleteResources } from "@/lib/db/queries";
 
 export async function GET() {
-  try {
-    await deleteResources();
-
-    return NextResponse.json({ result: "Resources deleted" }, { status: 200 });
-  } catch {
-    return NextResponse.json(
-      { result: "Error deleting resources" },
-      { status: 500 }
-    );
+  if (process.env.NODE_ENV === "production") {
+    return NextResponse.json({ status: 403 });
   }
+  // try {
+  //   await deleteResources();
+
+  //   return NextResponse.json({ result: "Resources deleted" }, { status: 200 });
+  // } catch {
+  //   return NextResponse.json(
+  //     { result: "Error deleting resources" },
+  //     { status: 500 }
+  //   );
+  // }
 }
