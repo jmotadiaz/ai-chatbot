e7466fd3c977a2bb3489d2f1db4c7eff95515999
jmotadiaz <javier.mota.dev@gmail.com>
2025-07-23 12:33:04 +0200

    improve rag upload loading

--- app/(chat)/rag/upload/loading.tsx
+++ app/(chat)/rag/upload/loading.tsx
@@ -1,7 +1,25 @@
 import { RAGUploadComponent } from "@/app/(chat)/rag/upload/component";
+import { Header } from "@/components/header";
+import { Logo } from "@/components/logo";
+import { NewChat } from "@/components/new-chat";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Main } from "@/components/ui/main";
 
 const Loading: React.FC = async () => {
-  return <RAGUploadComponent />;
+  return (
+    <Main>
+      <Header.Container>
+        <Header.Left>
+          <Logo />
+          <NewChat />
+        </Header.Left>
+        <Header.Right>
+          <ThemeToggle />
+        </Header.Right>
+      </Header.Container>
+      <RAGUploadComponent />
+    </Main>
+  );
 };
 
 export default Loading;
