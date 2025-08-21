03f73d3c5dd0e35d0c86706c01c05d98aa5adf09
jmotadiaz <javier.mota.dev@gmail.com>
2025-08-21 17:27:22 +0200

    force prefetch new chat

--- components/new-chat.tsx
+++ components/new-chat.tsx
@@ -5,6 +5,7 @@ import Link from "@/components/ui/link";
 export const NewChat = () => {
   return (
     <Link
+      prefetch={true}
       href="/"
       className="p-2 text-zinc-600 dark:text-zinc-300 hover:bg-zinc-200 dark:hover:bg-zinc-700 rounded-md transition-colors cursor-pointer"
     >
