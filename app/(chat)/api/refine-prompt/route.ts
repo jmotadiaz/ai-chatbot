3a051f3ddc5c887f319c2825cdc4626657333b22
jmotadiaz <164089051+jmotadiaz@users.noreply.github.com>
2025-08-22 10:12:18 +0200

    Update route.ts

--- app/(chat)/api/refine-prompt/route.ts
+++ app/(chat)/api/refine-prompt/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: Request) {
   const initialPrompt = chatHistory ? chatHistoryPrompt(chatHistory) : "";
 
   const { text } = await generateText({
-    ...languageModelConfigurations["GPT OSS"],
+    ...languageModelConfigurations["Qwen 3"],
     system: metaPrompt + metaPromptInputFormat + metaPromptOutputFormat,
     prompt: initialPrompt + originalPrompt(prompt),
   });
