94f0b45f1316b2699f3dfc5a8b32e44a32e61da7
jmotadiaz <javier.mota.dev@gmail.com>
2025-08-28 16:22:28 +0200

    remove conditional max duration

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -39,7 +39,7 @@ import { ragFactory } from "@/lib/ai/tools/rag";
 import { ChatbotMessage } from "@/lib/ai/types";
 import { hasUrls } from "@/lib/utils";
 
-export const maxDuration = process.env.NODE_ENV === "development" ? 60 * 5 : 60;
+export const maxDuration = 60;
 
 export async function POST(req: Request) {
   const session = await auth();
