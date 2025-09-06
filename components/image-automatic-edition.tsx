67f93955b9b1a062368bf7effaa53a16230f1143
jmotadiaz <javier.mota.dev@gmail.com>
2025-09-06 19:07:02 +0200

    improve image editor presentation

--- components/image-automatic-edition.tsx
+++ components/image-automatic-edition.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useDropzone } from "react-dropzone";
+import { ArrowDown } from "lucide-react";
 import { Content } from "@/components/content";
 import { convertFileToDataURLs } from "@/lib/ai/utils";
-import { Button } from "@/components/ui/button";
 import { useGeneratedText } from "@/lib/ai/hooks/use-generated-text";
 import { DotsLoadingIcon } from "@/components/icons";
 
@@ -23,34 +23,38 @@ export const ImageAutomaticEdition: React.FC = () => {
     });
   return (
     <Content className="pb-10">
-      <div
-        {...getRootProps()}
-        className="flex items-center p-10 border-2 border-dashed border-gray-300 rounded-md cursor-pointer"
-      >
-        <input {...getInputProps()} />
-        {filesInput.map((file, index) => (
-          <Image
-            width={50}
-            height={50}
-            className="w-10 h-auto mr-4 rounded-lg"
-            key={index}
-            src={file.url}
-            alt="optimized"
-          />
-        ))}
-        <p>Drag n drop some files here, or click to select files</p>
+      <div className="flex gap-4 items-stretch my-8">
+        <div
+          {...getRootProps()}
+          className="relative flex-1 p-12 border-2 border-dashed border-gray-300 rounded-md cursor-pointer"
+        >
+          <input {...getInputProps()} />
+          {filesInput.length ? (
+            filesInput.map((file, index) => (
+              <Image
+                width={50}
+                height={50}
+                className="absolute left-1/2 top-1/2 -translate-1/2 h-18 w-auto mr-4 rounded-lg"
+                key={index}
+                src={file.url}
+                alt="optimized"
+              />
+            ))
+          ) : (
+            <p>Drag n drop some files here, or click to select files</p>
+          )}
+        </div>
+        <button
+          className="w-1/4 bg-secondary rounded-2xl hover:bg-secondary-accent active:bg-secondary-accent/80 cursor-pointer"
+          disabled={filesInput.length === 0 || isLoading}
+          onClick={() => {
+            clear();
+            generate({ prompt: "Edit attached image" });
+          }}
+        >
+          <ArrowDown className="h-5 w-5 mx-auto" />
+        </button>
       </div>
-      <Button
-        variant="outline"
-        className="mt-4"
-        disabled={filesInput.length === 0 || isLoading}
-        onClick={() => {
-          clear();
-          generate({ prompt: "Edit attached image" });
-        }}
-      >
-        Submit
-      </Button>
       <div className="mt-8 flex justify-center">
         {isLoading && <DotsLoadingIcon size={6} />}
         <>
