73448d3d0e42720db2f59633b5c23b825fb176d6
jmotadiaz <javier.mota.dev@gmail.com>
2025-09-16 09:53:30 +0200

    add auth check to image editor

--- app/(chat)/image-editor/page.tsx
+++ app/(chat)/image-editor/page.tsx
@@ -1,9 +1,11 @@
 import { ImageEditorLayout } from "@/app/(chat)/image-editor/component";
 import { Sidebar } from "@/app/(chat)/sidebar";
+import { AuthCheck } from "@/components/auth-check";
 
 const ImageEditorPage: React.FC = () => {
   return (
     <>
+      <AuthCheck />
       <Sidebar />
       <ImageEditorLayout />
     </>
