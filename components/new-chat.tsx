ec9273819f5f8d62a0598dd53739dcee9a38dbce
jmotadiaz <javier.mota.dev@gmail.com>
2025-10-29 13:01:53 +0100

    improve temporary chat link

--- components/new-chat.tsx
+++ components/new-chat.tsx
@@ -66,7 +66,9 @@ export const NewChatSidebar: React.FC<NewChatSidebarProps> = ({
           </Item>
         </NewChat>
         <NewChat temporary>
-          <MessageCircleDashed className="h-5 w-5" />
+          <div className="p-2">
+            <MessageCircleDashed className="h-5 w-5" />
+          </div>
         </NewChat>
       </div>
     </Suspense>
