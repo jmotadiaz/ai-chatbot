19f80c087dead49677b15f864acedf1ac1c9d7de
jmotadiaz <164089051+jmotadiaz@users.noreply.github.com>
2025-11-01 00:51:07 +0100

    Update response.tsx

--- components/response.tsx
+++ components/response.tsx
@@ -6,38 +6,6 @@ import { Streamdown } from "streamdown";
 import addClasses from "rehype-class-names";
 import { cn } from "@/lib/utils";
 
-const components: StreamdownProps["components"] = {
-  table: ({ children, ...props }) => {
-    return (
-      <div className="mt-6 mb-8 w-full overflow-x-auto">
-        <table className="table-auto w-auto text-sm" {...props}>
-          {children}
-        </table>
-      </div>
-    );
-  },
-  th: ({ children, ...props }) => {
-    return (
-      <th
-        className="whitespace-nowrap text-left py-2 px-3 first-of-type:pl-0 last-of-type:pr-0 border-b border-secondary break-word"
-        {...props}
-      >
-        {children}
-      </th>
-    );
-  },
-  td: ({ children, ...props }) => {
-    return (
-      <td
-        className="py-2 px-3 first-of-type:pl-0 last-of-type:pr-0 border-b border-secondary wrap-break-word"
-        {...props}
-      >
-        {children}
-      </td>
-    );
-  },
-};
-
 export const Response = memo(
   ({ className, ...props }: StreamdownProps) => (
     <Streamdown
@@ -45,7 +13,6 @@ export const Response = memo(
         "size-full animate-streaming [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
         className
       )}
-      components={components}
       rehypePlugins={[
         [addClasses, { "h1,h2,h3,h4,h5,h6,p,li,code,pre": "animate-fade" }],
       ]}
