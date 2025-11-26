e5a75d8a5e25b67638c842ae37da0704922e558c
jmotadiaz <javier.mota.dev@gmail.com>
2025-11-26 13:06:15 +0100

    add claude opus 4.5

--- lib/ai/models/definition.ts
+++ lib/ai/models/definition.ts
@@ -189,8 +189,8 @@ const LANGUAGE_MODEL_CONFIGURATIONS_CONST = {
       },
     },
   },
-  "Claude Opus 4.1": {
-    model: providers.gateway("anthropic/claude-opus-4.1"),
+  "Claude Opus 4.5": {
+    model: providers.gateway("anthropic/claude-opus-4.5"),
     company: "anthropic",
     supportedFiles: ["img", "pdf"],
     reasoning: true,
@@ -365,6 +365,7 @@ export const chatModelKeys = [
   "Sonar Pro",
   "Claude Haiku 4.5",
   "Claude Sonnet 4.5",
+  "Claude Opus 4.5",
   "Grok 4.1 Fast",
   "Grok 4",
   "GPT 5 Mini",
