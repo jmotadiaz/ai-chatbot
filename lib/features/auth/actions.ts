7ea723b49db55c7ca8b871fa95b9e8bde03172e4
jmotadiaz <javier.mota.dev@gmail.com>
2025-12-13 00:35:31 +0100

    refactor: feature auth

---
+++ lib/features/auth/actions.ts
@@ -0,0 +1,120 @@
+"use server";
+
+import { z } from "zod";
+import { redirect } from "next/navigation";
+import { signIn } from "@/lib/features/auth/auth-config";
+import { transaction } from "@/lib/db/queries";
+import { createUser, getUser } from "./queries";
+
+const authFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export interface LoginActionState {
+  status:
+    | "idle"
+    | "in_progress"
+    | "success"
+    | "invalid_credentials"
+    | "invalid_data";
+}
+
+const redirectAfterLogin = (signInUrl: string) => {
+  const callbackUrl = `${signInUrl}`.split("callbackUrl=")[1];
+
+  if (callbackUrl) {
+    redirect(decodeURIComponent(callbackUrl));
+  }
+
+  redirect("/");
+};
+
+export const login = async (
+  _: unknown,
+  formData: FormData
+): Promise<LoginActionState> => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let signInUrl: any;
+  let signInStatus: LoginActionState = { status: "idle" };
+  try {
+    const validatedData = authFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    signInUrl = await signIn("credentials", {
+      email: validatedData.email,
+      password: validatedData.password,
+      redirect: false,
+    });
+    signInStatus = { status: "success" };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      signInStatus = { status: "invalid_data" };
+    } else {
+      signInStatus = { status: "invalid_credentials" };
+    }
+  }
+
+  if (signInStatus.status === "success") {
+    redirectAfterLogin(signInUrl);
+  }
+
+  return signInStatus;
+};
+
+export interface RegisterActionState {
+  status:
+    | "idle"
+    | "in_progress"
+    | "success"
+    | "signup_failed"
+    | "user_exists"
+    | "invalid_data";
+}
+
+export const register = async (
+  _: unknown,
+  formData: FormData
+): Promise<RegisterActionState> => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let signInUrl: any;
+  let signInStatus: RegisterActionState = { status: "idle" };
+  try {
+    const validatedData = authFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+
+    const users = await getUser(validatedData.email);
+    const [user] = users;
+
+    if (!user) {
+      await transaction(
+        createUser(validatedData.email, validatedData.password)
+      );
+      signInUrl = await signIn("credentials", {
+        email: validatedData.email,
+        password: validatedData.password,
+        redirect: false,
+      });
+      signInStatus = { status: "success" };
+    } else {
+      signInStatus = { status: "user_exists" };
+    }
+  } catch (error) {
+    console.error("Error registering", error);
+    if (error instanceof z.ZodError) {
+      signInStatus = { status: "invalid_data" };
+    } else {
+      signInStatus = { status: "signup_failed" };
+    }
+  }
+
+  if (signInStatus.status === "success") {
+    redirectAfterLogin(signInUrl);
+  }
+
+  return signInStatus;
+};
---
+++ lib/features/auth/queries.ts
@@ -0,0 +1,25 @@
+import "server-only";
+import { eq } from "drizzle-orm";
+import { user, type User } from "@/lib/db/schema";
+import { generateHashedPassword } from "@/lib/db/utils";
+import { getDb } from "@/lib/db/db";
+import type { Transactional } from "@/lib/db/queries";
+
+export function getUser(email: string): Promise<Array<User>> {
+  try {
+    return getDb().select().from(user).where(eq(user.email, email));
+  } catch (error) {
+    console.error("Failed to get user from database");
+    throw error;
+  }
+}
+
+export const createUser =
+  (email: string, password: string): Transactional<Array<User>> =>
+  (tx) => {
+    const hashedPassword = generateHashedPassword(password);
+    return tx
+      .insert(user)
+      .values({ email, password: hashedPassword })
+      .returning();
+  };
